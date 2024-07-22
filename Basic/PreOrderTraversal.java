184f2e83b9989e20d20a34ea13e6b0bcafbd0cf4
Mohd Saud <157240150+azmisaud@users.noreply.github.com>
2024-07-22 14:20:29 +0530

    Create PreOrderTraversal.java

---
+++ Basic/PreOrderTraversal.java
@@ -0,0 +1,18 @@
+class PreOrderTraversal {
+
+  static void preHelp(Node node, ArrayList<Integer> list) {
+    if(node==null)
+      return;
+
+    list.add(node.data);
+    preHelp(node.left,list);
+    preHelp(node.right,list);
+  }
+
+  static ArrayList<Integer> preOrder(Node root){
+    ArrayList<Integer> list=new ArrayList<>();
+
+    preHelp(root,list);
+
+    return list;
+  }
