61172614a44d1aa2b1fbafb536e80d94bc735589
Mohd Saud <157240150+azmisaud@users.noreply.github.com>
2024-07-25 11:51:17 +0530

    Create SumOfRightLeafNode.java

---
+++ Easy/SumOfRightLeafNode.java
@@ -0,0 +1,36 @@
+class SumOfRightLeafNode
+{
+    int rightLeafSum(Node root)
+    {
+        // code here
+        if(root.left==null && root.right==null)
+            return root.data;
+            
+        int sum=0;
+        
+        Queue<Node> queue=new LinkedList<>();
+        queue.add(root);
+        
+        while(!queue.isEmpty()){
+            int size=queue.size();
+            
+            for(int i=0;i<size;i++){
+            Node current=queue.poll();
+            
+            if(current.left!=null)
+                queue.add(current.left);
+                
+            if(current.right!=null && current.right.left==null && current.right.right==null){
+                sum+=current.right.data;
+                continue;
+            }
+            
+                
+            if(current.right!=null)
+                queue.add(current.right);
+            }
+        }
+        
+        return sum;
+    }
+}
