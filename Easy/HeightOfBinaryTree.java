ca224c45a149d95f2b26a8ea08d425071563dac3
Mohd Saud <157240150+azmisaud@users.noreply.github.com>
2024-07-25 11:12:57 +0530

    Create HeightOfBinaryTree.java

---
+++ Easy/HeightOfBinaryTree.java
@@ -0,0 +1,16 @@
+class Solution {
+    //Function to find the height of a binary tree.
+    int height(Node node) 
+    {
+        // code here 
+        if(node==null)
+            return 0;
+            
+        int left=height(node.left);
+        int right=height(node.right);
+        
+        return 1+Math.max(left,right);
+    }
+}
+
+// Height of binary tree is same as maximum depth of binary tree.
