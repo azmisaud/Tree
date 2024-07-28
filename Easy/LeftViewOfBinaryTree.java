557f06811e15a38c86791620a1a5ba6ca498f5b2
Mohd Saud <157240150+azmisaud@users.noreply.github.com>
2024-07-28 12:44:53 +0530

    Create LeftViewOfBinaryTree.java

---
+++ Easy/LeftViewOfBinaryTree.java
@@ -0,0 +1,48 @@
+/* A Binary Tree node
+class Node
+{
+    int data;
+    Node left, right;
+
+    Node(int item)
+    {
+        data = item;
+        left = right = null;
+    }
+}*/
+class LeftViewOfBinaryTree
+{
+    //Function to return list containing elements of left view of binary tree.
+    ArrayList<Integer> leftView(Node root)
+    {
+      // Your code here
+      
+      if(root==null)
+        return new ArrayList<>();
+        
+      Queue<Node> queue=new LinkedList<>();
+      
+      queue.add(root);
+      
+      ArrayList<Integer> result=new ArrayList<>();
+      
+     while(!queue.isEmpty()){
+         int size=queue.size();
+         
+         for(int i=0;i<size;i++){
+             Node current=queue.poll();
+             
+             if(i==0)
+                result.add(current.data);
+                
+            if(current.left!=null)
+                queue.add(current.left);
+                
+            if(current.right!=null)
+                queue.add(current.right);
+         }
+     }
+     
+     return result;
+    }
+}
