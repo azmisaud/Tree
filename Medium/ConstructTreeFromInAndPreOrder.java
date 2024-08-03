8efc1a6a6f70702403bd1de2660061bee87b82fe
Mohd Saud <157240150+azmisaud@users.noreply.github.com>
2024-08-03 13:17:58 +0530

    Create ConstructTreeFromInAndPreOrder.java

---
+++ Medium/ConstructTreeFromInAndPreOrder.java
@@ -0,0 +1,29 @@
+class ConstructTreeFromInAndPreOrder{
+
+  private static Node getTree(int[] inOrder, int[] preOrder, int root, int left, int right){
+    if(left>right || root>preOrder.length){
+      return null;
+    }
+
+    int rootPosition=0;
+
+    for(int i=left;i<=right;i++){
+      if(inOrder[i]=preOrder[root]){
+        rootPosition=i;
+        break;
+      }
+    }
+
+    Node result=new Node(inOrder[rootPosition]);
+    result.left=getTree(inOrder,preOrder,root+1,left,rootPosition-1);
+    result.right=getTree(inOrder,preOrder,root+rootPosition-left+1,rootPosition+1,right);
+
+    return result;
+  }
+
+  public static Node buildTree(int inorder[], int preorder[], int n)
+    {
+        // code here 
+        return getTree(inorder,preorder,0,0,n-1);
+    }
+}
