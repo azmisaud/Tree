d5ee2ead48dc69d8c58c33bd4be9c2241fc664cd
Mohd Saud <157240150+azmisaud@users.noreply.github.com>
2024-08-03 18:49:49 +0530

    Create IPL2021MatchDay.java

---
+++ Medium/IPL2021MatchDay.java
@@ -0,0 +1,112 @@
+/*
+// node structure:
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+*/
+
+class IPL2021MatchDay
+{
+    
+    private static void markParents(Node root, Map<Node,Node> parent){
+        Queue<Node> queue=new LinkedList<>();
+        queue.add(root);
+
+        parent.put(root,null);
+
+        while(!queue.isEmpty()){
+            int size=queue.size();
+
+            for(int i=0;i<size;i++){
+                Node current=queue.poll();
+
+                if(current.left!=null){
+                    queue.add(current.left);
+                    parent.put(current.left,current);
+                }
+
+                if(current.right!=null){
+                    queue.add(current.right);
+                    parent.put(current.right,current);
+                }
+            }
+        }
+    }
+    
+    private static Node findNode(Node root, int target){
+        if(root==null)
+            return null;
+
+        if(root.data==target)
+            return root;
+
+        Node left=findNode(root.left,target);
+        if(left!=null)
+            return left;
+
+        return findNode(root.right,target);
+    }
+    
+    
+    static int sum_at_distK(Node root, int target, int k){
+        // Your code goes here
+        
+        Map<Node,Node> parent=new HashMap<>();
+        markParents(root,parent);
+
+        Node targetNode=findNode(root,target);
+
+        Queue<Node> queue=new LinkedList<>();
+        queue.add(targetNode);
+
+        Set<Node> visited=new HashSet<>();
+        visited.add(targetNode);
+
+        int level=0;
+
+        int sum=0;
+        
+        while(!queue.isEmpty()){
+            if(level==k+1)
+                break;
+
+            int size=queue.size();
+
+            for(int i=0;i<size;i++){
+                Node current=queue.poll();
+
+                sum+=current.data;
+
+                if(current.left!=null && !visited.contains(current.left)){
+                    queue.add(current.left);
+                    visited.add(current.left);
+                }
+
+                if(current.right!=null && !visited.contains(current.right)){
+                    queue.add(current.right);
+                    visited.add(current.right);
+                }
+
+                if(parent.get(current)!=null && !visited.contains(parent.get(current))){
+                    queue.add(parent.get(current));
+                    visited.add(parent.get(current));
+                }
+            }
+
+            level++;
+        }
+
+
+        return sum;
+    }
+}
+
