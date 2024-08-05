c20119caa0ad0fe29eb9c5639bcc45df31817f1f
Mohd Saud <157240150+azmisaud@users.noreply.github.com>
2024-08-05 10:00:53 +0530

    Create SumOfLeafNodesBST.java

---
+++ Easy/SumOfLeafNodesBST.java
@@ -0,0 +1,25 @@
+/*node class of the binary ssearch tree
+class Node
+{
+    int data;
+    Node left, right;
+    Node(int key)
+    {
+        data = key;
+        left = right = null;
+    }
+}*/
+class SumOfLeafNodesBST
+{
+    public static int sumOfLeafNodes(Node root)
+    {
+        // code here 
+        if(root==null)
+            return 0;
+            
+        if(root.left==null && root.right==null)
+            return root.data;
+            
+        return sumOfLeafNodes(root.left)+sumOfLeafNodes(root.right);
+    }
+}
